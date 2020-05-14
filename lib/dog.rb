5669cb1de0c2de092990379b825495e77efc9a81
DAZUN XING <dazun.xing@gmail.com>
2020-05-14 21:44:48 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog
+  
+  # def initialize(name,breed)
+  #   @name=name
+  #   @breed=breed
+  # end
+  
+  
+  def name=(newdogsname)
+    @name =newdogsname
+  end
+  
+  def name
+    @name
+  end
+  
+  
+  def breed=(newbreedname)
+    @breed =newbreedname
+  end
+  
+  def breed
+    @breed
+  end
+  
+end
