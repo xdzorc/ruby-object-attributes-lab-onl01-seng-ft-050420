b7b4faabe10350882e1168b4a83a551e4ca89408
DAZUN XING <dazun.xing@gmail.com>
2020-05-14 21:43:27 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,19 @@
-class
+class Person
+  
+  def name =(name)
+    @name =name
+  end
+  
+  def name
+    @name
+  end
+  
+   def job =(job)
+    @name =job
+  end
+  
+   def job
+    @job
+  end
+  
+end
